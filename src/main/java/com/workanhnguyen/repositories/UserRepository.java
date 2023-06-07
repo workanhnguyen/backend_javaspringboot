d3ff147c49b819100455ecb30e12b040f34e7e48
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-06-07 15:16:14 +0700

    first commit

---
+++ src/main/java/com/workanhnguyen/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.workanhnguyen.repositories;
+
+import com.workanhnguyen.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/test/java/com/workanhnguyen/JavaSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workanhnguyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
